a8a215a36e2f5403f8bfd5b08910cd16d21e8444
AlekseeyTrifonov22 <bobikbobik2022@mail.ru>
2024-09-18 12:20:07 +0300

    init commit

---
+++ das.py
@@ -0,0 +1,11 @@
+x1 = int(input("Введите значение x1:"))
+x2 = int(input("Введите значение x2:"))
+x3 = int(input("Введите значение x3:"))
+sum = x1 + x2 + x3
+print(f"{x1} + {x2} + {x3} = {sum}")
+defference = x1 - x2 - x3
+print(f"{x1} - {x2} - {x3} = {defference}")
+composition = x1 * x2 * x3
+print(f"{x1} * {x2} * {x3}= {composition}")
+share = x1 / x2 / x3
+print(f"{x1} / {x2} / {x3} = {share}")
